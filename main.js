10e93cca01744b34215ca38b97060c38065855b1
Noah <44048306+FakeDazon@users.noreply.github.com>
2022-07-19 01:03:46 -0700

    styling and typing indicator

--- main.js
+++ main.js
@@ -2,10 +2,44 @@ var peerId = "hyp-" + Math.floor(Math.random() * 1000000)
 var peer = new Peer(peerId);
 var nm = ""
 var hosting = false
+var typing = false
+var typingTimeout
 peer.on('open', function(id) {
     console.log('My peer ID is: ' + id);
 });
 let conxs = []
+
+function textChange() {
+    clearTimeout(typingTimeout)
+    if (typing == false) {
+        typing = true
+        typingBeginMessage()
+
+    }
+    if (window.event.keyCode === 13) {
+        send()
+    }
+    typingTimeout = setTimeout(() => {
+        typing = false
+        typingEndMessage()
+    }, 2000);
+}
+
+function typingBeginMessage() {
+    var msg = new Message("typingStart")
+    conxs.forEach(conx => {
+        conx.send(msg)
+    })
+
+}
+
+function typingEndMessage() {
+    var msg = new Message("typingEnd")
+    conxs.forEach(conx => {
+        conx.send(msg)
+    })
+
+}
 
 function host() {
     hosting = true
@@ -30,15 +64,21 @@ peer.on('connection', function(conn) {
 });
 
 function send() {
+    typingEndMessage()
     var msg = new Message("message", h.get("#text").value)
+    h.get("#text").value = ""
     conxs.forEach(conx => {
         conx.send(msg)
     })
     h.get('#messages').innerHTML += `
-        <div class="font-bold ${msg.host == "true"
-    ? "text-sky-400" : ""}">${msg.name}:</div>
-        <div>${msg.data}</div>
-        <div class="divider"></div> 
+    <div class="flex flex-row flex-wrap shrink-0">
+    <div class="font-bold text-sky-400 inline shrink-0">${msg.host == "true"
+    ? "[HOST]&nbsp;" : ""}</div>
+        <div class="font-bold inline shrink-0 ${msg.host == "true"
+    ? "text-sky-400" : ""}">${msg.name}:&nbsp;</div>
+        <div class="inline shrink-0 whitespace-normal max-w-fit break-all ">&nbsp;${msg.data}</div>
+        </div>
+        <div class="divider my-0"></div> 
     `
     h.get("#messages").scrollTop = h.get("#messages").scrollHeight;
 
@@ -54,26 +94,37 @@ function newConnection() {
     })
 }
 
+var currentlyTyping = []
+
 function receivedMessage(msg) {
     console.log("recieved", msg)
     if (msg.type == "message") {
         h.get('#messages').innerHTML += `
-        <div class="font-bold text-sky-400">${msg.host == "true"
+        <div class="flex flex-row flex-wrap shrink-0">
+        <div class="font-bold text-sky-400 inline shrink-0">${msg.host == "true"
         ? "[HOST]&nbsp;" : ""}</div>
-            <div class="font-bold ${msg.host == "true"
+            <div class="font-bold inline shrink-0 ${msg.host == "true"
         ? "text-sky-400" : ""}">${msg.name}:&nbsp;</div>
-            <div>${msg.data}</div>
-            <div class="divider"></div> 
+            <div class="inline shrink-0 whitespace-normal max-w-fit break-all ">&nbsp;${msg.data}</div>
+            </div>
+            <div class="divider my-0"></div> 
         `
 
     } else if (msg.type == "connection") {
         h.get('#messages').innerHTML += `
             <div class="font-bold ${msg.host == "true"
         ? "text-sky-400" : ""}">${msg.name} joined</div>
-            <div class="divider"></div> 
+            <div class="divider my-0"></div> 
         `
 
+    } else if (msg.type == "typingStart") {
+        currentlyTyping.push(msg.name)
+        rebuildTypingUi()
+    } else if (msg.type == "typingEnd") {
+        currentlyTyping.splice(currentlyTyping.indexOf(msg.name), 1)
+        rebuildTypingUi()
     }
+    h.get("#messages").scrollTop = h.get("#messages").scrollHeight;
     console.log(msg.host, msg.user)
     if (hosting) {
         conxs.forEach(conx => {
@@ -84,12 +135,26 @@ function receivedMessage(msg) {
     }
 }
 
+function rebuildTypingUi() {
+    var names = ""
+    currentlyTyping.forEach(e => {
+        names += (e + " ")
+    })
+    h.get('#typing').innerHTML = ""
+    if (currentlyTyping.length == 0) return
+    h.get('#typing').innerHTML += `
+        <div class="font-bold">${names} ${currentlyTyping.length > 1 ? "are" : 'is'} currently typing</div>
+        <div class="divider my-0"></div> 
+    `
+
+}
+
 class Message {
     constructor(type, msg) {
         this.type = type
         this.user = peerId
-        this.data = msg
+        this.data = msg || ""
         this.host = hosting ? "true" : "false"
-        this.name = nm
+        this.name = nm || "host"
     }
 }
